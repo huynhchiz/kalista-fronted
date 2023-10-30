101c0252e0d238b06fa1e1f85b4740033bd6e388
huynhchiz <huynhchi90524@gmail.com>
2023-10-30 17:05:08 +0700

    tiep tuc

--- src/components/Posting/Posting.js
+++ src/components/Posting/Posting.js
@@ -7,80 +7,72 @@ import { useSelector } from 'react-redux'
 import { themeSelector } from '../../redux/selector'
 import { useState } from 'react'
 import BigButton from '../re-use/BigButton/BigButton'
-import { uploadImage as uploadImageSV} from '../../service/imageService'
+import { uploadImage as uploadImageSV } from '../../service/imageService'
+import axios from 'axios'
 
 const Posting = () => {
     const darkTheme = useSelector(themeSelector)
 
-    const [image, setImage] = useState({
+    const [file, setFile] = useState({
         src: '',
-        urlUpload: {}
+        type: ''
     })
-    const [video, setVideo] = useState({
-        src: '',
-        urlUpload: {}
-    })
+    const [fileUpload, setFileUpload] = useState()
     const [caption, setCaption] = useState('')
 
     const handleChangeFile = e => {
         let inputFile = e.target.files[0]
         if (inputFile) {
             if(inputFile.type.includes('video')) {
-                setVideo({
+                setFile({
                     src: URL.createObjectURL(inputFile),
-                    urlUpload: inputFile
+                    type: 'video'
                 })
             }
             if(inputFile.type.includes('image')) {
-                setImage({
+                setFile({
                     src: URL.createObjectURL(inputFile),
-                    urlUpload: inputFile
+                    type: 'image'
                 })
             }
-        }
+
+            // const reader = new FileReader();
+            // reader.readAsDataURL(inputFile);
+            // reader.onloadend = () => {
+            //     setFileUpload(reader.result)
+            // }
+
+            setFileUpload(inputFile)
+        }       
     }
 
-    console.log('urlUpload: ', image.urlUpload);
-
     const handleClosePreview = () => {
-        setImage('')
-        setVideo('')
+        setFile('')
     }
 
     const handleChangeCaption = e => {
         setCaption(e.target.value)
     }
 
-    const convertBase64 = (file) => { // file = image url or video url
-        return new Promise((resolve, reject) => {
-            const fileReader = new FileReader();
-            fileReader.onloadend = () => {
-                resolve(fileReader.result);
-            };
-            fileReader.readAsDataURL(file);
-
-            fileReader.onerror = (error) => {
-                reject(error);
-            };
-        });
-
-        // const fileReader = new FileReader()
-        // fileReader.readAsDataURL(file)
-        // fileReader.onloadend = () => {
-        //     return fileReader.result
-        // }
-    }
-
     const uploadImage = async () => {
-        if (image.urlUpload) {
-            let base64 = await convertBase64(image.urlUpload)
+        // if(fileUpload) {
+        //     let res = await uploadImageSV(fileUpload)
+        //     if(res && +res.EC === 0) {
+        //         console.log(res.DT);
+        //     }
+        // }
 
-            console.log({ base64 });
-            let res = await uploadImageSV(base64)
-            if(res && +res.EC === 0) {
-                console.log(res.DT);
-            }
-        }
+
+        const formdata = new FormData()
+        if (fileUpload)  formdata.append('image', fileUpload)
+        // let res = await uploadImageSV(formdata)
+        // if(res && +res.EC === 0) {
+        //     console.log(res.DT);
+        // }
+
+        axios.post('http://localhost:3333/api/image/upload', formdata)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
     }
 
     return (
@@ -90,11 +82,10 @@ const Posting = () => {
 
                 <div className='posting-preview'>
                     {
-                        image.src || video.src ?
+                        file.src ?
                         <>
-                            {image.src && <img alt='preview' src={image.src} />}
-
-                            {video.src && <video alt='preview' src={video.src} autoPlay/>}
+                            {file.type === 'image' && <img alt='preview' src={file.src} />}
+                            {file.type === 'video' && <video alt='preview' src={file.src} autoPlay/>}
 
                             <div className='close-preview-btn' onClick={handleClosePreview}>
                                 <FontAwesomeIcon icon={faCircleXmark} />
@@ -105,7 +96,13 @@ const Posting = () => {
                             <label className='posting-upload' htmlFor='upload-file'>
                                 <FontAwesomeIcon icon={faPlus} />
                             </label>
-                            <input id='upload-file' type='file' hidden onChange={handleChangeFile}/>
+                            <input  
+                                id='upload-file'
+                                type='file'
+                                name="upload-file"
+                                hidden 
+                                onChange={handleChangeFile}
+                            />
                         </>
                     }
                 </div>
@@ -118,7 +115,7 @@ const Posting = () => {
                     />
                 </div>
 
-                {image.src || video.src ?
+                {file.src ?
                     <div className='posting-button'>
                         <BigButton onClick={uploadImage}>SHARE POST</BigButton>
                     </div>
